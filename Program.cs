e545f87e3f143446f9e58e1a1f3f6f57e86a02b9
sanjana k <sanjanarthi@gmail.com>
2021-03-04 16:09:42 +0530

    "Added UC6"

--- Program.cs
+++ Program.cs
@@ -6,31 +6,38 @@ namespace Employee_Wages_Problem
     {
         static void Main(string[] args)
         {
-
             const int IS_PART_TIME = 1;
             const int IS_FULL_TIME = 2;
-            int EMP_RATE_PER_HOUR = 20;
+            const int EMP_RATE_PER_HOUR = 20;
+            const int NUM_OF_WORKING_DAYS = 2;
+
             // constants
             //variables
             int empHrs = 0;
             int empWage = 0;
-            Random random = new Random();
-            int empCheck = random.Next(0, 3);
-            switch (empCheck)
+            int totalEmpWage = 0;
+            for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
             {
-                case IS_PART_TIME:
-                    empHrs = 4;
-                    break;
-                case IS_FULL_TIME:
-                    empHrs = 8;
-                    break;
-                default:
-                    empHrs = 0;
-                    break;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                empWage = empHrs * EMP_RATE_PER_HOUR;
+                totalEmpWage += empWage;
+                Console.WriteLine("Emp wage:" + empWage);
             }
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Emp wage:" + empWage);
-            Console.ReadKey();
+            Console.WriteLine("Total Emp Wage:" + totalEmpWage);
+
 
         }
     }
