5fabbaa62db52f62110c005fe48c531f6b5a4983
sanjana k <sanjanarthi@gmail.com>
2021-03-08 09:37:29 +0530

    "Added UC - 7"

--- Program.cs
+++ Program.cs
@@ -5,42 +5,51 @@ namespace Employee_Wages_Problem
     class Program
     {
         static void Main(string[] args)
-        {
+       {
 			const int IS_PART_TIME = 1;
 			const int IS_FULL_TIME = 2;
 			const int EMP_RATE_PER_HOUR = 20;
 			const int NUM_OF_WORKING_DAYS = 2;
 			const int MAX_HRS_IN_MONTH = 10;
 
-			// constants
-			//variables
-			int empHrs = 0;
-			int totalEmpHrs = 0;
-			int totalWorkingDays = 0;
-			// computation
-			while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
+			static int computeEmpWage()
 			{
-				totalWorkingDays++;
-				Random random = new Random();
-				int empCheck = random.Next(0, 3);
-				switch (empCheck)
+				//variables
+
+				int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+				//computation
+
+				while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
 				{
-					case IS_PART_TIME:
-						empHrs = 4;
-						break;
-					case IS_FULL_TIME:
-						empHrs = 8;
-						break;
-					default:
-						empHrs = 0;
-						break;
+					totalWorkingDays++;
+					Random random = new Random();
+					int empCheck = random.Next(0, 3);
+					switch (empCheck)
+					{
+						case IS_PART_TIME:
+							empHrs = 4;
+							break;
+						case IS_FULL_TIME:
+							empHrs = 8;
+							break;
+						default:
+							empHrs = 0;
+							break;
+					}
+					totalEmpHrs += empHrs;
+					Console.WriteLine("day#:" + totalWorkingDays + " Emp Hrs :" + empHrs);
 				}
-				totalEmpHrs += empHrs;
-				Console.WriteLine("day#:" + totalWorkingDays + " Emp Hrs :" + empHrs);
+				int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+				Console.WriteLine("total emp wage:" + totalEmpWage);
+				return totalEmpWage;
 			}
-			int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-			Console.WriteLine("total emp wage:" + totalEmpWage);
+
+			static void main(string[] args)
+			{
+				computeEmpWage();
+			}
 		}
+
 
 	}
 }
